777155258a34c215dd19aaed8b166b39c0a1f6f1
Christo du Toit <christo@dutoit.co.uk>
2022-01-05 22:07:36 +0000

    BASE: Label Closes #3

---
+++ G2H.Portal.Web/Views/Bases/LabelBase.razor.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace G2H.Portal.Web.Views.Bases
+{
+    public partial class LabelBase : ComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+    }
+}
