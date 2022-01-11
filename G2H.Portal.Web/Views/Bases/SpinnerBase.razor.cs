133aa6ba8cd4292cf4287097042ebc7237dd905d
Christo du Toit <christo@dutoit.co.uk>
2022-01-11 18:56:25 +0000

    BASE: Spinner Closes #9

---
+++ G2H.Portal.Web/Views/Bases/SpinnerBase.razor.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace G2H.Portal.Web.Views.Bases
+{
+    public partial class SpinnerBase
+    {
+        [Parameter]
+        public bool IsVisible { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+
+        public void Show()
+        {
+            IsVisible = true;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Hide()
+        {
+            IsVisible = false;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void SetValue(string value)
+        {
+            Value = value;
+            InvokeAsync(StateHasChanged);
+        }
+    }
+}
