497a4c32fdac2dd3afc4d84b50eedd2da453e952
Christo du Toit <christo@dutoit.co.uk>
2022-01-31 20:17:14 +0000

    BROKERS: GET Post Type By Id Closes #31

--- G2H.Portal.Web/Brokers/Apis/ApiBroker.PostTypes.cs
+++ G2H.Portal.Web/Brokers/Apis/ApiBroker.PostTypes.cs
@@ -7,6 +7,7 @@
 // https://mark.bible/mark-16-15 
 // --------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using G2H.Portal.Web.Models.PostTypes;
@@ -19,5 +20,8 @@ namespace G2H.Portal.Web.Brokers.Apis
 
         public async ValueTask<List<PostType>> GetAllPostTypesAsync() =>
             await this.GetAsync<List<PostType>>(postTypesRelativeUrl);
+
+        public async ValueTask<PostType> GetPostTypeByIdAsync(Guid postTypeId) =>
+            await this.GetAsync<PostType>($"{postTypesRelativeUrl}/{postTypeId}");
     }
 }
