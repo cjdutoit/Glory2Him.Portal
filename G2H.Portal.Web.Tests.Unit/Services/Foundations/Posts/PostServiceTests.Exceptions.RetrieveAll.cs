416d5b28fb1d87e80965677bf370d610bc390f67
Christo du Toit <christo@dutoit.co.uk>
2022-02-07 16:02:07 +0000

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync -> FAIL

--- G2H.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RetrieveAll.cs
+++ G2H.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RetrieveAll.cs
@@ -102,5 +102,42 @@ namespace G2H.Portal.Web.Tests.Unit.Services.Foundations.Posts
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            var serviceException = new Exception();
+
+            var failedPostServiceException =
+                new FailedPostServiceException(serviceException);
+
+            var expectedPostServiceException =
+                new PostServiceException(failedPostServiceException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.GetAllPostsAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<List<Post>> retrieveAllPostsTask =
+                this.postService.RetrieveAllPostsAsync();
+
+            // then
+            await Assert.ThrowsAsync<PostServiceException>(() =>
+               retrieveAllPostsTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.GetAllPostsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostServiceException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
