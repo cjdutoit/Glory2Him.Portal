ac2edb1c37edf9ddc24d439f81897874046a4da6
Christo du Toit <christo@dutoit.co.uk>
2022-02-07 15:00:13 +0000

    BROKERS: Log Trace

---
+++ G2H.Portal.Web/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+namespace G2H.Portal.Web.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogTrace(string message);
+    }
+}
---
+++ G2H.Portal.Web/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging;
+
+namespace G2H.Portal.Web.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) => this.logger = logger;
+
+        public void LogTrace(string message) => this.logger.LogTrace(message);
+    }
+}
