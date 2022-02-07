2618cf6b259a7d0a580fb8940b0a95ad2dd18da6
Christo du Toit <christo@dutoit.co.uk>
2022-02-07 15:17:57 +0000

    BROKERS: Log Critical

--- G2H.Portal.Web/Brokers/Loggings/LoggingBroker.cs
+++ G2H.Portal.Web/Brokers/Loggings/LoggingBroker.cs
@@ -28,5 +28,8 @@ namespace G2H.Portal.Web.Brokers.Loggings
 
         public void LogError(Exception exception) =>
             this.logger.LogError(exception, exception.Message);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
     }
 }
