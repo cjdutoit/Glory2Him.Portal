7bc7a38ddcffc53b96a79b3b026d63614e46dc12
Christo du Toit <christo@dutoit.co.uk>
2022-02-07 14:51:59 +0000

    ShouldRetrieveAllPostsAsync -> FAIL

---
+++ G2H.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using G2H.Api.Web.Models.Posts;
+using G2H.Portal.Web.Brokers.Apis;
+using G2H.Portal.Web.Foundations.Posts;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace G2H.Portal.Web.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly IPostService postService;
+
+        public PostServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.postService = new PostService(apiBroker: this.apiBrokerMock.Object);
+        }
+
+        private static List<Post> CreateRandomPosts() =>
+            CreatePostFiller().Create(count: GetRandomNumber()).ToList();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Post> CreatePostFiller()
+        {
+            var filler = new Filler<Post>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset());
+
+            return filler;
+        }
+    }
+}
---
+++ G2H.Portal.Web/Foundations/Posts/PostService.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.Posts;
+using G2H.Portal.Web.Brokers.Apis;
+
+namespace G2H.Portal.Web.Foundations.Posts
+{
+    public partial class PostService : IPostService
+    {
+        private readonly IApiBroker apiBroker;
+
+        public PostService(
+            IApiBroker apiBroker)
+        {
+            this.apiBroker = apiBroker;
+        }
+
+        public ValueTask<List<Post>> RetrieveAllPostsAsync() =>
+            throw new System.NotImplementedException();
+    }
+}
